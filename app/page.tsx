903b032127718fdd99fa783b7381b8be319196ca
jaemjeon <piepie1346@gmail.com>
2024-10-11 05:15:34 +0900

    add mainpage image

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main>
       <div>
-        <p>
-          HOME PAGE!!!!
-        </p>
+        <Image 
+          src="/images/codingcat1.webp"
+          alt="main image"
+          layout="responsive"
+          width={4}
+          height={3}/>
       </div>
     </main>
   );
